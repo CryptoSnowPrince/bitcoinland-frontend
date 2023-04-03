c3c978aceb91407df3a6728e9155940f9441e8e4
CryptoSnowPrince <topdirector2017@gmail.com>
2023-04-04 04:36:18 +0900

    add pacakges

--- src/hooks/useBitcoinWallet.tsx
+++ src/hooks/useBitcoinWallet.tsx
@@ -1,19 +1,21 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
+import { StacksSessionState, authenticate } from 'micro-stacks/connect'
+import create from 'zustand'
+import bitcoin from '@btcd.io/bitcoinjs-lib'
+
+export const appDetails = {
+    name: 'Bitcoin Land',
+    icon: `bitcoinland/public/favicon.ico`,
+}
 
 const useBitcoinWallet = () => {
+
     const unisat = (window as any).unisat;
 
-    const [unisatInstalled, setUnisatInstalled] = useState(false);
     const [connected, setConnected] = useState(false);
-    const [accounts, setAccounts] = useState<string[]>([]);
     const [publicKey, setPublicKey] = useState("");
     const [address, setAddress] = useState("");
-    const [balance, setBalance] = useState({
-        confirmed: 0,
-        unconfirmed: 0,
-        total: 0,
-    });
-    const [network, setNetwork] = useState("livenet");
+    const [signMessage, setSignMessage] = useState();
 
     const getBasicInfo = async () => {
         const unisat = (window as any).unisat;
@@ -22,66 +24,55 @@ const useBitcoinWallet = () => {
 
         const publicKey = await unisat.getPublicKey();
         setPublicKey(publicKey);
-
-        const balance = await unisat.getBalance();
-        setBalance(balance);
-
-        const network = await unisat.getNetwork();
-        setNetwork(network);
     };
 
     const selfRef = useRef<{ accounts: string[] }>({
         accounts: [],
     });
     const self = selfRef.current;
-    const handleAccountsChanged = useCallback((_accounts: string[]) => {
-        if (self.accounts[0] === _accounts[0]) {
-            // prevent from triggering twice
-            return;
-        }
-        self.accounts = _accounts;
-        if (_accounts.length > 0) {
-            setAccounts(_accounts);
-            setConnected(true);
-
-            setAddress(_accounts[0]);
-
-            getBasicInfo();
-        } else {
-            setConnected(false);
+    const handleAccountsChanged = useCallback((name: string, data: string[] | StacksSessionState | any) => {
+        switch (name) {
+            case 'Unisat':
+                const _accounts = data as string[]
+                if (self.accounts[0] === _accounts[0]) {
+                    // prevent from triggering twice
+                    return;
+                }
+                self.accounts = _accounts;
+                if (_accounts.length > 0) {
+                    setConnected(true);
+        
+                    setAddress(_accounts[0]);
+        
+                    getBasicInfo();
+                    setSignMessage((window as any).unisat.signMessage)
+                } else {
+                    setConnected(false);
+                }
+                break;
+            case 'Xverse':
+                const _sessionXverse = data as any
+                if(!_sessionXverse) return;
+                if(_sessionXverse.profile.btcAddress.p2tr.mainnet || _sessionXverse.profile.btcPublickey.p2tr.mainnet) {
+                    setConnected(true);
+                    setAddress(_sessionXverse.profile.btcAddress.p2tr.mainnet)
+                    setPublicKey(_sessionXverse.profile.btcPublickey.p2tr.mainnet)
+                    setSignMessage((window as any).unisat.signMessage)
+                }
+                break;
+            case 'Hiro':
+                const _sessionHiro = data as any
+                if(!_sessionHiro) return;
+                break;
+            default:
+                break;
         }
     }, [self]);
 
-    // const handleNetworkChanged = (network: string) => {
-    //     setNetwork(network);
-    //     getBasicInfo();
-    // };
-
-    // useEffect(() => {
-    //     const unisat = (window as any).unisat;
-    //     if (unisat) {
-    //         setUnisatInstalled(true);
-    //     } else {
-    //         return;
-    //     }
-    //     unisat.getAccounts().then((accounts: string[]) => {
-    //         handleAccountsChanged(accounts);
-    //     });
-
-    //     unisat.on("accountsChanged", handleAccountsChanged);
-    //     unisat.on("networkChanged", handleNetworkChanged);
-
-    //     return () => {
-    //         unisat.removeListener("accountsChanged", handleAccountsChanged);
-    //         unisat.removeListener("networkChanged", handleNetworkChanged);
-    //     };
-    // }, []);
-
-    const signMessage = (window as any).unisat.signMessage;
     const wallets = [
-        // 'Xverse',
+        'Xverse',
         'Unisat',
-        // 'Hiro'
+        'Hiro'
     ];
 
     const disconnect = useCallback(async () => {
@@ -94,14 +85,26 @@ const useBitcoinWallet = () => {
                 console.log('connect')
                 const result = await unisat.requestAccounts();
                 console.log('result', result)
-                handleAccountsChanged(result);
+                handleAccountsChanged(name, result);
+                break;
+            case 'Xverse':
+                const _sessionXverse = await authenticate({ appDetails })
+                console.log('session', _sessionXverse)
+                if (!_sessionXverse) throw new Error('invalid session')
+                handleAccountsChanged(name, _sessionXverse);
+                break;
+            case 'Hiro':
+                const _sessionHiro = await authenticate({ appDetails })
+                console.log('session', _sessionHiro)
+                if (!_sessionHiro) throw new Error('invalid session')
+                handleAccountsChanged(name, _sessionHiro);
                 break;
             default:
                 break;
         }
     }, [unisat, handleAccountsChanged]);
 
-    return { connected, account: {address: address, publicKey: publicKey}, signMessage, wallets, connect, disconnect }
+    return { connected, account: { address: address, publicKey: publicKey }, signMessage, wallets, connect, disconnect }
 };
 
 export default useBitcoinWallet;
